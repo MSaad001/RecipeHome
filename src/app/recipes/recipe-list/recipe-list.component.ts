4b1f8a28fbe10c32d3525e2ed4997e7bf33e99ad
MSaad001 <sm7226336@gmail.com>
2024-05-15 23:53:55 +0500

    recipes added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('TestRecipe', 'This is a test recipe', 'https://images.freeimages.com/images/large-previews/4e6/food-1329024.jpg'),
+    new Recipe('Chicken Pulao', 'The tasty adventure starts when you smell the great chicken pulao being cooked nearby', 'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/recipes/021d144c10c84d9b91fdfaf8068ce39e.png?resize=1000:*&output-format=auto&output-quality=auto')
+  ];
 }
